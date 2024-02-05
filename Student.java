c5565e41eaff6269fd40864c7ff0e4f2f674344b
Ethan-H147 <97569719+Ethan-H147@users.noreply.github.com>
2024-02-04 23:20:14 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,12 +1,19 @@
+/**
+ * Ethan Hu
+ * 2023/02/04
+ * Senior Seminar
+ * Student Class-information of a student
+ */
+
 import java.util.ArrayList;
 
 public class Student {
-	private String nameString;
-	private Seminar[] choices;
-    private Seminar[] attends=new Seminar[5];
+	private String nameString;//name
+	private Seminar[] choices;//what they chose
+    private Seminar[] attends=new Seminar[5];//what they actually attend
     private int ns=0;
     
-
+    //constructor
     public Student(String name, Seminar[] choice){
         nameString = name;
         choices = choice;
@@ -14,7 +21,7 @@ public class Student {
             attends[i]=null;
         }
     }
-    
+    //number of unscheduled blocks
     public int ns(){
         for(int i=0;i<5;i++){
             if(attends[i].getNum()==19){
@@ -23,7 +30,7 @@ public class Student {
         }
         return ns;
     }
-
+    
     public String getName(){
         return nameString;
     }
@@ -35,17 +42,17 @@ public class Student {
     public Seminar[] getAttends(){
         return attends;
     }
-
+    //set a block in their schedule as a seminar
     public void setSchedule(int i, Seminar a){
         attends[i]=a;
         a.attend();
     }
 
-
+    //print their choices
     public String toString() {
         return nameString+" "+choices[0].getNum()+" "+choices[1].getNum()+" "+choices[2].getNum()+" "+choices[3].getNum()+" "+choices[4].getNum();
     }
-
+    //print their schedule
     public String schedule(){
         return nameString+" "+attends[0].getNum()+" "+attends[1].getNum()+" "+attends[2].getNum()+" "+attends[3].getNum()+" "+attends[4].getNum();
     }
